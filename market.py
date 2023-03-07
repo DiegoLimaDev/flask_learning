8d12bcc7418ac46c094854ae5c7cf6f59f308a99
Diego Lima <xxdiegolsxx@gmail.com>
2023-03-06 22:19:17 -0300

    [feat]templates folder

--- market.py
+++ market.py
@@ -1,10 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_wolrd():
-    return '<h1>Hello world mudou</h1>'
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
 
-@app.route('/about/<username>')
-def about_page(username):
-    return f'<h1>About page of the user: {username}</h1>'
